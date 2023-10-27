46150d14b6c8d77d39147150908790103f6a13a3
BrendonTheDev <108491991+BrendonTheDev@users.noreply.github.com>
2023-10-27 12:29:57 -0400

    Music button works. Need to move component to util
    On first attempt to move component, got file not found.
    
    Issue with correct file path structure when accessing assets from within
    
    a directory seperate from the intended parent compnent.

--- restaurant-skyrim-project/src/pages/Home.jsx
+++ restaurant-skyrim-project/src/pages/Home.jsx
@@ -1,6 +1,35 @@
 import "./Home.css";
+import React from "react";
+import useSound from "use-sound";
+import song from "../assets/song.mp3";
 
 function Home() {
+  const MusicPlayer = () => {
+    const [isPlaying, setIsPlaying] = React.useState(false);
+
+    const [playBoop, { pause }] = useSound(song, {
+      onplay: () => setIsPlaying(true),
+      onend: () => setIsPlaying(false),
+    });
+
+    const togglePlay = () => {
+      if (isPlaying) {
+        pause();
+      } else {
+        playBoop();
+      }
+      setIsPlaying(!isPlaying);
+    };
+
+    return (
+      <>
+        <button className="btn-17  btn-music" onClick={togglePlay}>
+          Play Music
+        </button>
+      </>
+    );
+  };
+
   return (
     <div>
       <section className="wrapper">
@@ -14,7 +43,10 @@ function Home() {
                 <h2 className=" mx-auto mt-2 mb-5 sub-header">
                   An unforgettable dining experience.
                 </h2>
-                <button className="btn-17">View Menu</button>
+                <div className="btn-container">
+                  <MusicPlayer />
+                  <button className="btn-17">View Menu</button>
+                </div>
               </div>
             </div>
           </div>
