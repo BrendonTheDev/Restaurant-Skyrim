a8ace8f4c376c4ab3f873b7c81df3196c51d6be6
BrendonTheDev <108491991+BrendonTheDev@users.noreply.github.com>
2023-11-03 13:40:57 -0400

    Menu Grid finished, unless I decide to add header.

--- restaurant-skyrim-project/src/App.jsx
+++ restaurant-skyrim-project/src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
-import Menutwo from "./pages/Menu2";
 import Contact from "./pages/Contact";
 import Embers from "./pages/Embers";
 import "./App.css";
--- restaurant-skyrim-project/src/pages/Menu.jsx
+++ restaurant-skyrim-project/src/pages/Menu.jsx
@@ -1,4 +1,5 @@
 import "./Menu.css";
+import horker from "../assets/horkerStew.jpg";
 
 function Menu() {
   return (
@@ -6,55 +7,56 @@ function Menu() {
       <div className="card">
         <div id="horker" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Horker Stew</h5>
-          <p className="card-text">
+          <h3 className="card-title">Horker Stew</h3>
+          <h5 className="card-text">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
       <div className="card">
         <div id="seared" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Seared Slaughterfish</h5>
-          <p className="card-text">
+          <h3 className="card-title">Seared Slaughterfish</h3>
+          <h5 className="card-text">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
       <div className="card">
         <div id="skeever" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Charred Skeever</h5>
-          <p className="card-text">
+          <h3 className="card-title">Charred Skeever</h3>
+          <h5 className="card-text">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
       <div className="card">
         <div id="crab" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Steamed Mudcrab</h5>
-          <p className="card-text">
+          <h3 className="card-title">Steamed Mudcrab</h3>
+          <h5 className="card-text h">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
       <div className="card">
         <div id="cheese" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Eldar Cheese Wheel</h5>
-          <p className="card-text">
+          <h3 className="card-title">Eldar Cheese Wheel</h3>
+          <h5 className="card-text">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
       <div className="card">
+        <img src="" alt="" />
         <div id="sweetroll" className="img-container"></div>
         <div className="card-body">
-          <h5 className="card-title">Sweet Roll</h5>
-          <p className="card-text">
+          <h3 className="card-title">Sweet Roll</h3>
+          <h5 className="card-text">
             "Let Me Guess; Someone Stole Your Sweetroll?"
-          </p>
+          </h5>
         </div>
       </div>
     </div>
